bbc3cd1432380ebbfa80bb8214343aaedc16115c
Milos <cosma203@gmail.com>
2018-09-16 19:11:48 +0800

    Format timestamps using moment.js

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,9 +1,11 @@
+const moment = require('moment');
+
 var generateMessage = function(from, text) {
-  return { from: from, text: text, createdAt: new Date().getTime() };
+  return { from: from, text: text, createdAt: moment().valueOf() };
 };
 
 var generateLocationMessage = function(from, latitude, longitude) {
-  return { from: from, url: `https://google.com/maps?q=${latitude},${longitude}`, createdAt: new Date().getTime() };
+  return { from: from, url: `https://google.com/maps?q=${latitude},${longitude}`, createdAt: moment().valueOf() };
 };
 
 module.exports = { generateMessage, generateLocationMessage };
